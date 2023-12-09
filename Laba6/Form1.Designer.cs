e5e54ec2fa8236f8763bff5d20167e7d88768b31
M1lc3 <sofarua@gmail.com>
2023-12-09 18:28:56 +0200

    v0.1

---
+++ Laba6/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Laba6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw Hexagon";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(146, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Draw Circle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(13, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(127, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Draw Ellipse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(146, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Draw Rectangle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Location = new System.Drawing.Point(422, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 553);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(13, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(260, 57);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Draw Viecle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(13, 255);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(260, 57);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Draw Animated Ellipse";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(12, 318);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(127, 48);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Stop";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.button7.Hide();
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Location = new System.Drawing.Point(13, 408);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(260, 57);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Draw Animated Hammer";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Location = new System.Drawing.Point(13, 471);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(127, 48);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Stop";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            this.button9.Hide();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(1094, 577);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Laba6/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen myPen = new Pen(Color.White);
+            g.DrawRectangle(myPen, 1, 1, 1, 1);
+        }
+
+        Graphics g;
+        float coeff;
+        float fi;
+        int count = 0;
+        static RectangleF[] pEllipse =
+             {
+                 new RectangleF(200, 300,30,50),
+                 new RectangleF(300, 300,30,50),
+            };
+        RectangleF[] pEllipseFI = new RectangleF[2];
+        private void button1_Click(object sender, EventArgs e) // Draw Hexagon
+        {
+            int startPoint = 30;
+            Point[] points =
+             {
+                 new Point(startPoint, startPoint),
+                 new Point(startPoint - 10, startPoint +10),
+                 new Point(startPoint - 10, startPoint +20),
+                 new Point(startPoint, startPoint+30),
+                 new Point(startPoint+10, startPoint+20),
+                 new Point(startPoint+10, startPoint+10),
+                 new Point(startPoint-9, startPoint-9)
+            };
+            Random r = new Random();
+            int cr = r.Next(255);
+            int cg = r.Next(255);
+            int cb = r.Next(255);
+            Pen myPen = new Pen(Color.FromArgb(cr, cg, cb), 25);
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            g.DrawLines(myPen,points);
+        }
+        private void button2_Click(object sender, EventArgs e) // Draw Circle
+        {
+            Random r = new Random();
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            int cr = r.Next(255);
+            int cg = r.Next(255);
+            int cb = r.Next(255);
+            Pen myPen = new Pen(Color.FromArgb(cr, cg, cb), 5);
+            g.DrawEllipse(myPen, 430, 10, 50, 50);
+        }
+        private void button3_Click(object sender, EventArgs e) // Draw Ellipse
+        {
+            Random r = new Random();
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            int cr = r.Next(255);
+            int cg = r.Next(255);
+            int cb = r.Next(255);
+            Pen myPen = new Pen(Color.FromArgb(cr, cg, cb), 25);
+            g.DrawEllipse(myPen, 20, 410, 50, 20);
+        }
+        private void button4_Click(object sender, EventArgs e) // Draw Rectangle
+        {
+            Random r = new Random();
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            int cr = r.Next(255);
+            int cg = r.Next(255);
+            int cb = r.Next(255);
+            Pen myPen = new Pen(Color.FromArgb(cr, cg, cb), 5);
+            g.DrawRectangle(myPen, 380, 380, 100, 50);
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Draw Viecle
+        {
+            Random r = new Random();
+            g.Clear(Color.White);
+            Point[] points1 =
+            {
+                 new Point(190,104),
+                 new Point(160,104),
+                 new Point(160,75),
+                 new Point(190,75),
+                 new Point(218,50),
+                 new Point(280,50),
+                 new Point(300,75),
+                 new Point(325,75),
+                 new Point(325,104),
+                 new Point(303,104)
+            }; 
+            Point[] points2 =
+             {
+                 new Point(273,104),
+                 new Point(220,104)
+            };
+            int cr = r.Next(255);
+            int cg = r.Next(255);
+            int cb = r.Next(255);
+            Pen myPen = new Pen(Color.FromArgb(cr, cg, cb), 5);
+            g.DrawEllipse(myPen, 190, 90, 30, 30);
+            g.DrawEllipse(myPen, 270, 90, 30, 30);
+            g.DrawLines(myPen, points1);
+            g.DrawLines(myPen, points2);
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Draw Animated Ellipse
+        {
+            coeff = 0;
+            button7.Show();
+            timer1.Start();
+        }
+        private void button7_Click(object sender, EventArgs e) // Stop 1
+        {
+            g.Clear(Color.White);
+            button7.Hide();
+            timer1.Stop();
+            coeff = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // Timer 1 
+        {
+            g.Clear(Color.White);
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            Pen myPen = new Pen(Color.Black, 5);
+            fi = coeff * (float)Math.PI;
+            for (int i = 0; i < 2; i++)
+            {
+                pEllipseFI[i] = EllipseBuilder(fi, pEllipse[i]);
+                g.DrawEllipse(myPen, pEllipseFI[i]);
+            }
+            coeff += 0.05f;
+            if (coeff == 2)
+                coeff = 0;   
+        }
+        static RectangleF EllipseBuilder(float fi, RectangleF ptt)
+        {
+            float dx = ptt.X - (pEllipse[0].X + pEllipse[1].X)/2;
+            float dy = ptt.Y - (pEllipse[0].Y + pEllipse[1].Y) / 2;
+            float dxn = dx * (float)Math.Cos(fi) - dy * (float)Math.Sin(fi);
+            float dyn = dx * (float)Math.Sin(fi) + dy * (float)Math.Cos(fi);
+            RectangleF pt = new RectangleF(((pEllipse[0].X + pEllipse[1].X) / 2) + dxn, ((pEllipse[0].Y + pEllipse[1].Y) / 2) + dyn, 30, 50);
+            return pt;
+        }
+
+        private void button8_Click(object sender, EventArgs e) // Draw Animated Hammer
+        {
+            timer2.Start();
+            button9.Show();
+        }
+        private void button9_Click(object sender, EventArgs e) // Stop 2
+        {
+            g.Clear(Color.White);
+            timer2.Stop();
+            button9.Hide();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // Timer 2
+        {
+            g.Clear(Color.White);
+            Pen myPen = new Pen(Color.Black, 5);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 260, 15, 40); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 250, 40, 10); // Nail (hat) 
+            g.DrawRectangle(myPen, 320, 70, 20, 150); // Hammer (handle) |
+            g.DrawRectangle(myPen, 290, 10, 80, 60); // Hammer |
+            System.Threading.Thread.Sleep(500);
+
+            g.Clear(Color.White);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 265, 15, 35); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 255, 40, 10); // Nail (hat) 
+            g.DrawRectangle(myPen, 190, 200, 150, 20); // Hammer (handle) -
+            g.DrawRectangle(myPen, 130, 170, 60, 80); // Hammer -
+            System.Threading.Thread.Sleep(500);
+
+            g.Clear(Color.White);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 265, 15, 35); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 255, 40, 10); // Nail (hat)  
+            g.DrawRectangle(myPen, 320, 75, 20, 150); // Hammer (handle) |
+            g.DrawRectangle(myPen, 290, 15, 80, 60); // Hammer |
+            System.Threading.Thread.Sleep(500);
+
+            g.Clear(Color.White);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 270, 15, 30); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 260, 40, 10); // Nail (hat)  
+            g.DrawRectangle(myPen, 190, 205, 150, 20); // Hammer (handle) -
+            g.DrawRectangle(myPen, 130, 175, 60, 80); // Hammer -
+            System.Threading.Thread.Sleep(500);
+
+            g.Clear(Color.White);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 270, 15, 30); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 260, 40, 10); // Nail (hat) 
+            g.DrawRectangle(myPen, 320, 80, 20, 150); // Hammer (handle) |
+            g.DrawRectangle(myPen, 290, 20, 80, 60); // Hammer |
+            System.Threading.Thread.Sleep(500);
+
+            g.Clear(Color.White);
+            g.DrawRectangle(myPen, 60, 300, 300, 50); // Wood
+            g.DrawRectangle(myPen, 150, 275, 15, 25); // Nail (base) 
+            g.DrawRectangle(myPen, 137, 265, 40, 10); // Nail (hat)  
+            g.DrawRectangle(myPen, 190, 210, 150, 20); // Hammer (handle) -
+            g.DrawRectangle(myPen, 130, 180, 60, 80); // Hammer -
+            System.Threading.Thread.Sleep(500);
+        }
+    }
+}
